63f333d906268d0ceb6eae8c950f3c654cfc4613
Ryan <leachrp@comcast.net>
2018-08-19 21:29:28 -0700

    Updated css, js, and html

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,9 +2,24 @@ var listItemParent = document.querySelector("ul");
 listItemParent.addEventListener("click", strikeThroughText, false);
 
 function strikeThroughText(e) {
-    if (e.target !== e.currentTarget) {
+    if ((e.target !== e.currentTarget) && (/todoItem/.test(e.target.className)) ) {
         var clickedItem = e.target;
         clickedItem.classList.toggle("strikethrough");
     }
     e.stopPropagation();
 }
+
+$("ul").on("click", ".deleteButton", function fadeOutAndRemove(e) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(e){
+    if(e.which === 13) {
+        var newTodoText = $(this).val();
+        $("ul").append("<li class='todoItem'>" + newTodoText + "<span class='deleteButton'><i class='fas fa-trash'></i></span>" + "</li>")
+        $(this).val("");
+    }
+});
